2959d418b38aaf3594bb931ad2dc514cc5176b1b
Maxence Oden <maxenceoden@orange.fr>
2021-03-06 21:30:15 +0100

    Init network.h

---
+++ src/network/network.h
@@ -0,0 +1,13 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define USER_LIST_SIZE 100
+#define STATIC_DNS localhost
+
+
+
+#endif
