03e2e8cf91d04787a662d7eb5d5834ba734ed009
Nathan RABET <nathan.rabet@epita.fr>
2021-03-23 14:33:34 +0100

    Our bits_n operators suck, we will use OpenSSL

--- tests/main_test.c
+++ tests/main_test.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-#include "misc/bits_n_test.h"
-
 int main()
 {
-    add_bits_n_test();
     return 0;
 }
--- tests/misc/bits_n_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef BITS_N_TEST_H
-#define BITS_N_TEST_H
-
-
-void add_bits_n_test();
-#endif
