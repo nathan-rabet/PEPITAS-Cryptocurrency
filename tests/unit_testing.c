92effd4b6e4ec16f6c522255d186ce461d1da444
Nathan RABET <nathan.rabet@epita.fr>
2021-03-25 17:04:22 +0100

    .bin -> Wejdene said 'OUT OF MY VIEW'

--- tests/unit_testing.c
+++ tests/unit_testing.c
@@ -9,12 +9,11 @@
 
 int main()
 {
-    OpenSSL_add_all_algorithms();
     DEBUG(generate_key_test);
     DEBUG(get_keys_test);
 
     system("rm -rf ./.keys");
-    
+
     DEBUG(get_generate_keys_equality_test);
     DEBUG(verify_sign_test);
     return 0;
