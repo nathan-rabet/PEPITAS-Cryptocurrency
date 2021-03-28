27a0c919f2644ee700b654f062de24b138126e8d
Nathan RABET <nathan.rabet@epita.fr>
2021-03-28 20:31:08 +0200

    send_client_list : done

--- src/server.c
+++ src/server.c
@@ -3,10 +3,6 @@
 #include "network/client.h"
 
 int main()
-{
-    set_neighbour(NULL, 0);
-    get_my_node()->neighbours->hostname = "192.168.1.1";
-    get_my_node()->neighbours->family = AF_INET;
-    
+{   
     return init_server();
 }
