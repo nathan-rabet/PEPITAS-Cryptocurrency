3cca1217d82020886764aaecd5c9c0bda1256b80
Nathan RABET <nathan.rabet@epita.fr>
2021-04-07 00:53:13 +0200

    I can't believe it took so long to fix this.

--- src/server.c
+++ src/server.c
@@ -8,5 +8,8 @@ int main()
 {
     get_my_node()->neighbours[0].family = AF_INET;
     get_my_node()->neighbours[0].hostname = "78.191.117.199";
+
+    get_my_node()->neighbours[0].family = AF_INET6;
+    get_my_node()->neighbours[0].hostname = "2a02:842a:222:701:6a5:b521:f6bd:996b";
     return init_server();
 }
