9f8b49dc3f5952c5799a346e2b97f00b15d74814
Nathan RABET <nathan.rabet@epita.fr>
2021-04-07 02:02:50 +0200

    Doxygen friendly

--- tests/tests_macros.h
+++ tests/tests_macros.h
@@ -10,18 +10,18 @@
     printf("\033[0m\n")
 
 #define TEST_PASSED(name...)          \
-    printf("\033[0;32m[✓] TEST → '"); \
+    printf("\033[0;32m[OK] TEST -> '"); \
     printf(name);                     \
     printf("' success\033[0m\n")
 
 #define TEST_FAILED(name, reason...)                               \
-    printf("\033[0;31m[✗] TEST '%s' failed\n\t⤷ REASON : ", name); \
+    printf("\033[0;31m[x] TEST '%s' failed\n\t-> REASON : ", name); \
     printf(reason);                                                \
     printf("\033[0m\n");                                           \
     exit(1)
 
 #define TEST_WARNING(name, reason...)                          \
-    printf("\033[0;33m[!] WARNING '%s'\n\t⤷ BECAUSE : ", name); \
+    printf("\033[0;33m[!] WARNING '%s'\n\t-> BECAUSE : ", name); \
     printf(reason);                                            \
     printf("\033[0m\n")
 
