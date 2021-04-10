6d980ac0950b4f9f5b8a5970e5e8a87eac64f147
Maxence Oden <maxence.oden@epita.fr>
2021-04-10 18:25:35 +0200

    fix safe read

--- src/misc/safe.c
+++ src/misc/safe.c
@@ -23,9 +23,9 @@ ssize_t safe_read(int fd, const void **buf, size_t *bufsize)
 	do
 	{
 		ssize_t nb_read = read(fd, buffer + read_count, buffersize - read_count);
+		printf("%s", buffer);
 		if (nb_read == 0)
 		{
-			printf("Aye fini\n");
 			break;
 		}
 		
@@ -37,7 +37,7 @@ ssize_t safe_read(int fd, const void **buf, size_t *bufsize)
 			buffersize *= 2;
 			buffer = realloc(buffer, buffersize);
 		}
-	} while (strncmp("\r\n\r\n", buffer + (read_count - 5), 5) != 0);
+	} while (strncmp("\r\n\r\n", buffer + (read_count - 4), 4) != 0);
 	*buf = buffer;
 	*bufsize = buffersize;
 	return read_count;
