30904457246859bb73f53305e7eb6577d7319607
Maxence Oden <maxence.oden@epita.fr>
2021-05-27 21:48:29 +0200

    test new cmp key

--- headers/cryptosystem/rsa.h
+++ headers/cryptosystem/rsa.h
@@ -32,9 +32,24 @@ void get_keys(char *password);
  * 
  */
 static inline int cmp_public_keys(RSA *key1, RSA *key2){
-    return key1 && key2 && 
-    !BN_cmp(RSA_get0_n(key1),RSA_get0_n(key2)) &&
-    !BN_cmp(RSA_get0_e(key1),RSA_get0_e(key2));
+    if (key1 && key2)
+    {
+        char my_pkey_string1[RSA_FILE_TOTAL_SIZE];
+        char my_pkey_string2[RSA_FILE_TOTAL_SIZE];
+        BIO *my_pub1 = BIO_new(BIO_s_mem());
+        BIO *my_pub2 = BIO_new(BIO_s_mem());
+        PEM_write_bio_RSAPublicKey(my_pub1, key1);
+        PEM_write_bio_RSAPublicKey(my_pub2, key2);
+        BIO_read(my_pub1, my_pkey_string1, RSA_FILE_TOTAL_SIZE);
+        BIO_read(my_pub2, my_pkey_string2, RSA_FILE_TOTAL_SIZE);
+        BIO_free(my_pub1);
+        BIO_free(my_pub2);
+        return strncmp(my_pkey_string1, my_pkey_string2, RSA_FILE_TOTAL_SIZE) == 0;
+    }
+    return false;
+    // return key1 && key2 && 
+    // !BN_cmp(RSA_get0_n(key1),RSA_get0_n(key2)) &&
+    // !BN_cmp(RSA_get0_e(key1),RSA_get0_e(key2));
 }
 
 #endif
